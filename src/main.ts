1a0b30011049aa9827d556aa0697c8fb735fcf36
ymkz <ymkz@users.noreply.github.com>
2022-08-14 23:28:06 +0900

    feat: implement start server

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import fastify from 'fastify'
 import { loadConfig } from './config'
 
 const bootstrap = async () => {
+  const app = fastify()
   const config = await loadConfig(process.env.APP_ENV)
-  console.log(config)
+
+  app.get('/healthz', (_request, _reply) => {
+    return { status: 'UP' }
+  })
+
+  app.listen({ port: config.port }, (err) => {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+
+    console.log(`Ready on http://localhost:${config.port}`)
+  })
 }
 
 bootstrap()
